718575a498d312925ad76e836a088da83ae2f51e
Parth Tiwari <parthtiwari670@gmail.com>
2025-07-05 23:47:07 +0530

    add customer model with MongoDB connection and order schema

---
+++ Models/customer.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+main().then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log(err));
+
+async function main(){
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo')
+};
+
+const OrderSchema = new mongoose.Schema({
+  username: String,
+  addresses: [
+    {
+      _id: false, // Disable automatic _id generation for subdocuments
+      location: String,
+      city: String,
+    }
+  ]
+});
