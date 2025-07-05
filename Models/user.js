9c0a98c9d4b2ba2c497f2355208d1dd15511ff20
Parth Tiwari <parthtiwari670@gmail.com>
2025-07-05 12:26:03 +0530

    add user model and initial data insertion for MongoDB

---
+++ Models/user.js
@@ -0,0 +1,38 @@
+// ONE TO FEW RELATIONSHIP
+
+const mongoose = require('mongoose');
+
+main().then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log(err));
+
+async function main(){
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo')
+};
+
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  addresses: [
+    {
+      location: String,
+      city: String,
+    }
+  ]
+});
+
+const User = mongoose.model('User', userSchema);
+
+const addusers = async ()=>{
+  let user1 = new User({
+    username:'Parth Tiwari',
+    addresses:[
+      {location: 'Triveni Nagar 2 ',
+        city: 'Lucknow'
+      }]
+  });
+  user1.addresses.push({location: 'Triveni Nagar 3', city: 'Lucknow'});
+  let result = await user1.save();
+  console.log(result);
+}
+
+addusers();
